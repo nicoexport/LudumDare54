942d64a16994aaffeb6af206a5fb864edd460139
Nico Auerswald <auersw.nico@gmail.com>
2023-10-01 00:25:50 +0200

    feat: add component state behaviour

---
+++ Assets/Scripts/Character/StateBehaviour/CharacterMovementState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace LudumDare {
+    class CharacterMovementState : ComponentStateBehaviour<CharacterMovement> {
+        protected override void StateEnter(in AnimatorStateInfo stateInfo, int layerIndex) {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void StateExit(in AnimatorStateInfo stateInfo, int layerIndex) {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void StateUpdate(AnimatorStateInfo stateInfo, int layerIndex) => throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Character/StateBehaviour/ComponentStateBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace LudumDare {
+    public abstract class ComponentStateBehaviour<T> : StateMachineBehaviour where T : Component {
+        protected Animator attachedAnimator { get; private set; }
+        protected T attachedComponent { get; private set; }
+
+        public sealed override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo,
+            int layerIndex) {
+            attachedAnimator = animator;
+            animator.TryGetComponent(out T component);
+            attachedComponent = component;
+
+            Assert.IsTrue(attachedComponent, $"Animator {animator} does not have a {typeof(T)} component!");
+
+            StateEnter(stateInfo, layerIndex);
+        }
+
+        public sealed override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo,
+            int layerIndex) =>
+            StateExit(stateInfo, layerIndex);
+
+        public sealed override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo,
+            int layerIndex) {
+            if (animator.GetCurrentAnimatorStateInfo(layerIndex).fullPathHash == stateInfo.fullPathHash) {
+                StateUpdate(stateInfo, layerIndex);
+            }
+        }
+
+        protected abstract void StateEnter(in AnimatorStateInfo stateInfo, int layerIndex);
+
+        protected abstract void StateUpdate(AnimatorStateInfo stateInfo, int layerIndex);
+
+        protected abstract void StateExit(in AnimatorStateInfo stateInfo, int layerIndex);
+    }
+}
