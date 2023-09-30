f9956c1098bee1e89db305de039c736a1c0cb886
Nico Auerswald <auersw.nico@gmail.com>
2023-10-01 00:21:38 +0200

    implement attack intends

--- Assets/Scripts/CharacterAttack.cs
+++ Assets/Scripts/CharacterAttack.cs
@@ -2,6 +2,23 @@ using UnityEngine;
 
 namespace LudumDare {
     public class CharacterAttack : ComponentFeature<Animator> {
+        BoolBuffer intendBuffer = new();
 
+        public bool intendsAttack {
+            get => attachedComponent.GetBool(nameof(intendsAttack));
+            set => attachedComponent.SetBool(nameof(intendsAttack), value);
+        }
+
+        protected void Update() {
+            intendsAttack = intendBuffer.value;
+        }
+
+        protected void FixedUpdate() {
+            intendBuffer.Tick();
+        }
+
+        void OnAttack() {
+            intendBuffer.SetForFrames(3);
+        }
     }
 }
