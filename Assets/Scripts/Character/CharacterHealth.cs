447f6a1f9a13501ae91167b91ce32c26948fa772
Nico Auerswald <auersw.nico@gmail.com>
2023-10-01 00:59:04 +0200

    feat: implement dying

--- Assets/Scripts/Character/CharacterHealth.cs
+++ Assets/Scripts/Character/CharacterHealth.cs
@@ -1,9 +1,22 @@
+using MyBox;
 using UnityEngine;
 
 namespace LudumDare {
     public class CharacterHealth : MonoBehaviour {
+        [SerializeField]
+        int maxHealth = 50;
+        [SerializeField, ReadOnly]
+        int currentHealth;
+
+        protected void Start() {
+            currentHealth = maxHealth;
+        }
+
         public void TakeDamage(int amount) {
-            Debug.Log($"i took {amount} Damage!");
+            currentHealth -= amount;
+            if (currentHealth <= 0){
+                Destroy(gameObject);
+            }
         }
     }
 }
--- Assets/Scripts/HitBox.cs
+++ Assets/Scripts/HitBox.cs
@@ -7,12 +7,10 @@ namespace LudumDare.Assets.Scripts {
 
         protected void OnTriggerEnter2D(Collider2D collider) {
             if (!whatIsEnemy.IsInLayerMask(collider.gameObject)) {
-                Debug.Log($"not in layer {collider.gameObject.name}");
                 return;
             }
 
             if(!collider.TryGetComponent(out CharacterHealth health)) {
-                Debug.Log("no health component");
                 return;
             }
 
