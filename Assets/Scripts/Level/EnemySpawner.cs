7ecfbb6f103835849506c9fe5183138049e35f69
Nico Auerswald <auersw.nico@gmail.com>
2023-10-02 14:33:50 +0200

    add gizmos for spawn positions

--- Assets/Scripts/Level/EnemySpawner.cs
+++ Assets/Scripts/Level/EnemySpawner.cs
@@ -6,7 +6,15 @@ namespace LudumDare.Assets.Scripts.Level {
         [SerializeField] GameObject[] enemyPrefabs;
         [SerializeField] int totalNumberOfEnemies;
         [SerializeField] MinMaxInt spawnDelayInFrames;
-        
+        [Separator]
+        [SerializeField] Vector3[] spawnPositions;
 
+        protected void OnDrawGizmos() {
+            Gizmos.color = Color.red;
+
+            foreach (var pos in spawnPositions) {
+                Gizmos.DrawWireSphere(pos, 1f);
+            }
+        }
     }
 }
