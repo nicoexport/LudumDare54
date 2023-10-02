790cfe46f100ae7c0478e93b5e7b2564c64633c8
Nico Auerswald <auersw.nico@gmail.com>
2023-10-02 16:22:56 +0200

    setup level finish screen

---
+++ Assets/Scripts/UI/LevelFinishedScreen.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace LudumDare.Assets.Scripts.UI {
+    class LevelFinishedScreen : MonoBehaviour{
+        [SerializeField]
+        GameObject root;
+
+        [SerializeField]
+        Button continueButton;
+        [SerializeField]
+        Button mainMenuButton;
+        [SerializeField]
+        Button quitButton;
+
+        protected void OnEnable() {
+            continueButton.onClick.AddListener(Continue);
+            mainMenuButton.onClick.AddListener(MainMenu);
+            quitButton.onClick.AddListener(Quit);
+            DisableScreen();
+        }
+
+        void EnableScreen() {
+            root.SetActive(true);
+        }
+
+        void DisableScreen() {
+            root.SetActive(false);
+        }
+
+        void Continue() {
+            Debug.Log("Continue");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        void MainMenu() {
+            Debug.Log("Main Menu");
+        }
+
+        void Quit() {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
+    }
+}
