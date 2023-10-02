521fdc8c95eeff1477e369bd7e6e9ea5df0962af
Nico Auerswald <auersw.nico@gmail.com>
2023-10-02 19:59:53 +0200

    add percent to lifesteal

---
+++ Assets/Scripts/Character/LifeSteal.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace LudumDare.Assets.Scripts.Character {
+    class LifeSteal : MonoBehaviour {
+        [SerializeField] CharacterHealth health;
+        [SerializeField, Range(0f, 1f)] float percent;
+
+        public void Steal(int amount) {
+            health.GainHealth((int)(amount * percent));
+        }
+
+    }
+}
